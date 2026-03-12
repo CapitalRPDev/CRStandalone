5a07b51df9a9ff96248adb4c52d2503c64481717
HanssenGLRP <66432612+HanssenRP@users.noreply.github.com>
2026-03-12 15:08:09 +0100

    Postal Map
    - Added new british postal map

---
+++ bs-mapv3/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'British Studios'
+description 'British Studios - Postal Map'
+
+client_script 'client.lua'
+
+lua54 'yes'
+dependency '/assetpacks'
